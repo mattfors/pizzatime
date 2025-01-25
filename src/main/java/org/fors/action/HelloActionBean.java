34e5e74fd615b5274d0931e3e7bed34fd91a129b
Matthew Fors <1824866+mattfors@users.noreply.github.com>
2025-01-25 12:41:48 -0500

    #1 verify db from ibatis/stripes

--- src/main/java/org/fors/action/HelloActionBean.java
+++ src/main/java/org/fors/action/HelloActionBean.java
@@ -1,10 +1,51 @@
 package org.fors.action;
 
+import com.ibatis.sqlmap.client.SqlMapClient;
 import net.sourceforge.stripes.action.*;
+import org.fors.IBatisUtil;
+
+import javax.servlet.ServletContext;
+import java.util.List;
 
 @UrlBinding("/hello.action")
 public class HelloActionBean implements ActionBean {
     private ActionBeanContext context;
+    private String javaVersion;
+    private String serverVersion;
+    private String sqlVersion;
+    private List<String> schemas;
+
+    public String getServerVersion() {
+        return serverVersion;
+    }
+
+    public void setServerVersion(String serverVersion) {
+        this.serverVersion = serverVersion;
+    }
+
+    public String getJavaVersion() {
+        return javaVersion;
+    }
+
+    public void setJavaVersion(String javaVersion) {
+        this.javaVersion = javaVersion;
+    }
+
+    public String getSqlVersion() {
+        return sqlVersion;
+    }
+
+    public void setSqlVersion(String sqlVersion) {
+        this.sqlVersion = sqlVersion;
+    }
+
+    public List<String> getSchemas() {
+        return schemas;
+    }
+
+    public void setSchemas(List<String> schemas) {
+        this.schemas = schemas;
+    }
 
     @Override
     public ActionBeanContext getContext() {
@@ -18,6 +59,19 @@ public class HelloActionBean implements ActionBean {
 
     @DefaultHandler
     public Resolution hello() {
+        try {
+            SqlMapClient sqlMapClient = IBatisUtil.getSqlMapClient();
+            sqlVersion = (String) sqlMapClient.queryForObject("ExampleMapper.selectVersion");
+            javaVersion = System.getProperty("java.version");
+            serverVersion = getServletContext().getServerInfo();
+            schemas = sqlMapClient.queryForList("ExampleMapper.selectSchemas");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return new ForwardResolution("/WEB-INF/jsp/hello.jsp");
+    }
+
+    private ServletContext getServletContext() {
+        return context.getServletContext();
     }
 }
--- src/main/webapp/WEB-INF/jsp/hello.jsp
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -1,9 +1,20 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<jsp:useBean id="actionBean" scope="request" type="org.fors.action.HelloActionBean"/>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Hello Stripes</title>
+    <title>Pizza Time Running</title>
 </head>
 <body>
-<h1>Hello, Stripes!</h1>
+<h1>Pizza Time Running</h1>
+<p>SQL Version: ${actionBean.sqlVersion}</p>
+<p>Java Version: ${actionBean.javaVersion}</p>
+<p>Server Version: ${actionBean.serverVersion}</p>
+<p>Timestamp: <%= new java.util.Date() %></p>
+<ul>
+    <c:forEach var="schema" items="${actionBean.schemas}">
+        <li>${schema}</li>
+    </c:forEach>
+</ul>
 </body>
 </html>
